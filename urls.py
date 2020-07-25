5441637c4def2596f2cdf2ba3bcc2b5ab75c3be2
Shobhika <36703097+shobhikagarg@users.noreply.github.com>
2020-07-25 15:22:48 +0530

    Project files
    Project files

--- urls.py
+++ urls.py
@@ -1,17 +1,26 @@
+"""crud URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from django.urls import path,include
 
-urlpatterns=[
-    path('home',views.home,name='home'),
-    path('about/',views.about,name='about'),
-    path('list_item/',views.list_item,name='list_item'),
-    path('add_item/',views.add_item,name='add_item'),
-    path('<id>/delete_view',views.delete_view,name='delete_view'),
-    path('stock_detail/<id>/', views.stock_detail, name="stock_detail"),
-    path('signup/',views.signup,name='signup'),
-    path('login/',views.login,name='login'),
-    path('logout/',views.logout,name='logout'),
-path('issue_items/<str:pk>/', views.issue_items, name="issue_items"),
-path('receive_items/<str:pk>/', views.receive_items, name="receive_items"),]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('store.urls')),
+
+]
+urlpatterns += staticfiles_urlpatterns()
